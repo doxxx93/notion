d58909e3da9aea281ae86c7fdc564fb9229ee447
doxxx <doxxx93@gmail.com>
2023-09-04 10:12:53 +0900

    refactor: modify sample data initialization in PageService to use Random

--- src/main/java/me/doxxx/notion/page/PageService.java
+++ src/main/java/me/doxxx/notion/page/PageService.java
@@ -3,6 +3,10 @@ package me.doxxx.notion.page;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 @Service
 public class PageService {
@@ -13,50 +17,27 @@ public class PageService {
         pageRepository = new PageRepository();
     }
 
+    /**
+     * This method is used to initialize sample data for the application. It populates
+     * a list of pages with random titles and content. Each page, except for the first one,
+     * is assigned a random parent page from the existing pages list. The method also saves
+     * each page using the savePage() method.
+     */
     @PostConstruct
     private void initializeSampleData() {
-        Page samplePage1 = new Page("Title 1", "Content 1");
-        savePage(samplePage1);
+        List<Page> pages = new ArrayList<>();
+        Random random = new Random();
 
-        Page samplePage2 = new Page("Title 2", "Content 2");
-        samplePage1.addSubPage(samplePage2);
-        savePage(samplePage2);
+        for (int i = 1; i <= 10; i++) {
+            Page samplePage = new Page("Title " + i, "Content " + i);
+            if (i > 1) {
+                int parentIndex = random.nextInt(i - 1);
+                pages.get(parentIndex).addSubPage(samplePage);
+            }
 
-        Page samplePage3 = new Page("Title 3", "Content 3");
-        samplePage2.addSubPage(samplePage3);
-        savePage(samplePage3);
-
-        Page samplePage4 = new Page("Title 4", "Content 4");
-        samplePage3.addSubPage(samplePage4);
-        savePage(samplePage4);
-
-        Page samplePage5 = new Page("Title 5", "Content 5");
-        samplePage4.addSubPage(samplePage5);
-        savePage(samplePage5);
-
-        Page samplePage6 = new Page("Title 6", "Content 6");
-        samplePage4.addSubPage(samplePage6);
-        savePage(samplePage6);
-
-        Page samplePage7 = new Page("Title 7", "Content 7");
-        samplePage3.addSubPage(samplePage7);
-        savePage(samplePage7);
-
-        Page samplePage8 = new Page("Title 8", "Content 8");
-        samplePage7.addSubPage(samplePage8);
-        savePage(samplePage8);
-
-        Page samplePage9 = new Page("Title 9", "Content 9");
-        samplePage8.addSubPage(samplePage9);
-        savePage(samplePage9);
-
-        Page samplePage10 = new Page("Title 10", "Content 10");
-        samplePage2.addSubPage(samplePage10);
-        savePage(samplePage10);
-
-        Page samplePage11 = new Page("Title 11", "Content 11");
-        samplePage2.addSubPage(samplePage11);
-        savePage(samplePage11);
+            savePage(samplePage);
+            pages.add(samplePage);
+        }
     }
 
     public PageResponse getPageById(long pageId) {
